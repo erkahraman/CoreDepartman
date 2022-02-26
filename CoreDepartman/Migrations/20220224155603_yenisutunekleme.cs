f991cd4604c8c8286bf5375883f23389cf7e5576
erkahraman <kahramanerdinc@gmail.com>
2022-02-27 00:45:38 +0300

    Proje dosyası ekle.

---
+++ CoreDepartman/Migrations/20220224155603_yenisutunekleme.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CoreDepartman.Migrations
+{
+    public partial class yenisutunekleme : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "detay",
+                table: "departmanlars",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "detay",
+                table: "departmanlars");
+        }
+    }
+}
---
+++ CoreDepartman/Models/Context.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDepartman.Models
+{
+    public class Context:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=(localdb)\\MSSQLLocalDB; database=CorePersonel; integrated security=true;");
+            //base.OnConfiguring(optionsBuilder);
+        }
+        public DbSet<departmanlar> departmanlars { get; set; }
+        public DbSet<personel> personels { get; set; }
+    }
+}
